27e25e999f14f9053b08e6aefd43923c8b816c9e
MariaBezgodkova <ask.2004@yandex.ru>
2023-02-21 12:24:14 +0300

    Возведение числа А в степень В

---
+++ DZ25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// Math.Pow использовать нельзя
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+
+string StepenA(int A, int B)
+{
+    string result = "";
+    int newA = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        newA = newA * A;
+    }
+    result = result + newA;
+    return result;
+}
+
+Console.WriteLine("Введите два натуральных числа:");
+int A = int.Parse(Console.ReadLine()!);
+int B = int.Parse(Console.ReadLine()!);
+string newA2 = StepenA(A, B);
+Console.WriteLine(" число A в степени В равно:");
+Console.WriteLine(newA2);
